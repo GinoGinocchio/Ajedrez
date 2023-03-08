5befe37a6098e6166c8740d9d38e78510376501f
GinoGinocchio <gino.ginocchio@unmsm.edu.pe>
2023-03-08 14:27:46 -0500

    Aplicando el patron Command

--- src/Original.src/ChessMenuBar.java
+++ src/Original.src/ChessMenuBar.java
@@ -1,5 +1,7 @@
 import java.awt.Component;
 import java.awt.event.*;
+import java.util.HashMap;
+import java.util.Map;
 import javax.swing.*;
 // -------------------------------------------------------------------------
 /**
@@ -10,82 +12,73 @@ import javax.swing.*;
  * @author Danielle Bushrow (dbushrow)
  * @version 2010.11.17
  */
-public class ChessMenuBar
-    extends JMenuBar{
+public class ChessMenuBar extends JMenuBar {
+
+    private final Map<String, Command> commands = new HashMap<>();
+
     // ----------------------------------------------------------
     /**
      * Create a new ChessMenuBar object.
      */
-    public ChessMenuBar(){
-        String[] menuCategories = { "File", "Options", "Help" };
-        String[] menuItemLists =
-        { "New game/restart,Exit", "Toggle graveyard,Toggle game log",
-          "About" };
-        for ( int i = 0; i < menuCategories.length; i++ ){
-            JMenu currMenu = new JMenu( menuCategories[i] );
-            String[] currMenuItemList = menuItemLists[i].split( "," );
-            for ( int j = 0; j < currMenuItemList.length; j++ ){
-                JMenuItem currItem = new JMenuItem( currMenuItemList[j] );
-                currItem.addActionListener( new MenuListener() );
-                currMenu.add( currItem );
-            }
-            this.add( currMenu );
-        }
+    public ChessMenuBar() {
+        addCommand("About", this::aboutHandler);
+        addCommand("New game/restart", this::restartHandler);
+        addCommand("Toggle game log", this::toggleGameLogHandler);
+        addCommand("Exit", this::exitHandler);
+        addCommand("Toggle graveyard", this::toggleGraveyardHandler);
+
+        JMenu fileMenu = new JMenu("File");
+        fileMenu.add(createMenuItem("New game/restart"));
+        fileMenu.add(createMenuItem("Exit"));
+        add(fileMenu);
+
+        JMenu optionsMenu = new JMenu("Options");
+        optionsMenu.add(createMenuItem("Toggle graveyard"));
+        optionsMenu.add(createMenuItem("Toggle game log"));
+        add(optionsMenu);
+
+        JMenu helpMenu = new JMenu("Help");
+        helpMenu.add(createMenuItem("About"));
+        add(helpMenu);
     }
-    /**
-     * Listener for the north menu bar.
-     *
-     * @author Ben Katz (bakatz)
-     * @author Myles David II (davidmm2)
-     * @author Danielle Bushrow (dbushrow)
-     * @version 2010.11.17
-     */
-    private class MenuListener
-        implements ActionListener
-    {
-        /**
-         * Takes an appropriate action based on which menu button is clicked
-         *
-         * @param event
-         *            the mouse event from the source
-         */
-        @Override
-        public void actionPerformed( ActionEvent event ){
-            String buttonName = ( (JMenuItem)event.getSource() ).getText();
-            if ( buttonName.equals( "About" ) ){
-                aboutHandler();
-            }
-            else if ( buttonName.equals( "New game/restart" ) ){
-                restartHandler();
-            }
-            else if ( buttonName.equals( "Toggle game log" ) ){
-                toggleGameLogHandler();
-            }
-            else if ( buttonName.equals( "Exit" ) ){
-                exitHandler();
-            }
-            else
-            {
-                toggleGraveyardHandler();
-            }
-        }
+
+    private void addCommand(String name, Command command) {
+        commands.put(name, command);
     }
+
+    private JMenuItem createMenuItem(String name) {
+        JMenuItem item = new JMenuItem(name);
+        item.addActionListener(e -> {
+            Command command = commands.get(name);
+            if (command != null) {
+                command.execute();
+            }
+        });
+        return item;
+    }
+
+    private interface Command {
+        void execute();
+    }
+
     // ----------------------------------------------------------
     /**
      * Takes an appropriate action if the about button is clicked.
      */
-    private void aboutHandler(){
+    private void aboutHandler() {
         JOptionPane.showMessageDialog(
             this.getParent(),
             "YetAnotherChessGame v1.0 by:\nBen Katz\nMyles David\n"
                 + "Danielle Bushrow\n\nFinal Project for CS2114 @ VT" );
     }
+
     /**
      * Takes an appropriate action if the restart button is clicked.
      */
-    private void restartHandler(){
-        ( (ChessPanel)this.getParent() ).getGameEngine().reset();
+    private void restartHandler() {
+        ((ChessPanel)this.getParent()).getGameEngine().reset();
     }
+
     /**
      * Takes an appropriate action if the exit button is clicked.
      * Uses Tony Allevato's code for exiting a GUI app without System.exit()
@@ -100,22 +93,24 @@ public class ChessMenuBar
             jFrame.dispose();
         }
     }
-    
+
     /**
      * Takes an appropriate action if the toggle graveyard button is clicked.
      */
-    private void toggleGraveyardHandler(){
-        ( (ChessPanel)this.getParent() ).getGraveyard( 1 ).setVisible(
-            !( (ChessPanel)this.getParent() ).getGraveyard( 1 ).isVisible() );
-        ( (ChessPanel)this.getParent() ).getGraveyard( 2 ).setVisible(
-            !( (ChessPanel)this.getParent() ).getGraveyard( 2 ).isVisible() );
+    private void toggleGraveyardHandler() {
+        ((ChessPanel)this.getParent()).getGraveyard(1).setVisible(
+            !((ChessPanel)this.getParent()).getGraveyard(1).isVisible());
+        ((ChessPanel)this.getParent()).getGraveyard(2).setVisible(
+            !((ChessPanel)this.getParent()).getGraveyard(2).isVisible());
     }
+
     /**
      * Takes an appropriate action if the toggle game log button is clicked.
      */
-    private void toggleGameLogHandler(){
-        ( (ChessPanel)this.getParent() ).getGameLog().setVisible(
-            !( (ChessPanel)this.getParent() ).getGameLog().isVisible() );
-        ( (ChessPanel)this.getParent() ).revalidate();
+    private void toggleGameLogHandler() {
+        ((ChessPanel)this.getParent()).getGameLog().setVisible(
+            !((ChessPanel)this.getParent()).getGameLog().isVisible());
+        ((ChessPanel)this.getParent()).revalidate();
     }
 }
+
