ac824aad2bbf3b1a27ba3d4874243d154e17c62d
GinoGinocchio <gino.ginocchio@unmsm.edu.pe>
2023-03-08 10:46:32 -0500

    Añadiendo patron Singleton

--- src/Original.src/ChessMain.java
+++ src/Original.src/ChessMain.java
@@ -1,27 +1,27 @@
 import javax.swing.*;
 import javax.swing.WindowConstants;
-// -------------------------------------------------------------------------
-/**
- * Shows the GUI for the Chess game.
- * 
- * @author Ben Katz (bakatz)
- * @author Myles David II (davidmm2)
- * @author Danielle Bushrow (dbushrow)
- * @version 2010.11.17
- */
+
 public class ChessMain{
-    // ----------------------------------------------------------
-    /**
-     * Creates the GUI for Chess.
-     * 
-     * @param args
-     *            command line arguments, not used
-     */
-    public static void main( String[] args ){
+    private static ChessMain instance = null;
+
+    private ChessMain() {}
+
+    public static ChessMain getInstance() {
+        if (instance == null) {
+            instance = new ChessMain();
+        }
+        return instance;
+    }
+    
+    public void startGame() {
         JFrame frame = new JFrame( "YetAnotherChessGame 1.0" );
         frame.setDefaultCloseOperation( WindowConstants.EXIT_ON_CLOSE );
         frame.getContentPane().add( new ChessPanel() );
         frame.pack();
         frame.setVisible( true );
+    }
+
+    public static void main( String[] args ){
+        ChessMain.getInstance().startGame();
     }
 }
